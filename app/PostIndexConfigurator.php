35838a9d7a8b6cba6f4c4807baf9d097581b0edd
Shwe Sin <hnin.ssp@blueplanet.com.mm>
2020-03-15 11:56:36 +0630

    elastic search server complete

---
+++ app/PostIndexConfigurator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use ScoutElastic\IndexConfigurator;
+use ScoutElastic\Migratable;
+
+class PostIndexConfigurator extends IndexConfigurator
+{
+    use Migratable;
+
+    protected $name = 'padc9_post_index';
+
+    /**
+     * @var array
+     */
+    protected $settings = [
+        //
+    ];
+}
